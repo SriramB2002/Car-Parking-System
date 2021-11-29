7a0d25094ddaca64b46ccd8050e7dfebafcc22eb
SriramB2002 <f20200002@hyderabad.bits-pilani.ac.in>
2021-11-29 20:48:07 +0530

    Updated dashboards

--- svelte/src/UserDashboard.svelte
+++ svelte/src/UserDashboard.svelte
@@ -1,7 +1,10 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+</svelte:head>
+
 <script>
 import { navigate } from "svelte-navigator";
-
-
+import { Button, Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "sveltestrap/src";
     import { login } from "./stores";
 
     const signOut = () => {
@@ -16,10 +19,50 @@ import { navigate } from "svelte-navigator";
 </script>
 
 <main>
+    <Navbar color="dark" dark expand="md">
+        <NavbarBrand href="/UserDashboard">Car Parking System</NavbarBrand>
+        <button class="green" on:click={signOut}>Sign Out</button>
+    </Navbar>
     <p>{$login}</p>
-    <button on:click={signOut}>Sign Out</button>
+    <h1>Book your slot here</h1>
+    <h6>Location</h6>
+    <Dropdown>
+        <DropdownToggle caret>Location</DropdownToggle>
+        <DropdownMenu>
+            <DropdownItem>Delhi</DropdownItem>
+            <DropdownItem>Mumbai</DropdownItem>
+            <DropdownItem>Chennai</DropdownItem>
+            <DropdownItem>Hyderabad</DropdownItem>
+        </DropdownMenu>
+    </Dropdown>
+    <br>
+    <h6>Date</h6>
+    <input type="date"><br><br>
+    <h6>Checkin</h6>
+    <input type="time"><br><br>
+    <h6>Checkout</h6>
+    <input type="time"><br><br>
 </main>
 
 <style>
+    main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
 
+    h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+    button {
+        border-radius: 5px;
+    }
+
+    .green {
+        background-color: chartreuse;
+    }
 </style>
