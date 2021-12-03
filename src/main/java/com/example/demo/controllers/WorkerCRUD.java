3d70eaaea5a6b68f5725da6d1eeeda4f74663bee
plaidbait91 <f20200055@hyderabad.bits-pilani.ac.in>
2021-12-03 12:47:22 +0530

    Add/remove workers as admin

--- src/main/java/com/example/demo/controllers/WorkerCRUD.java
+++ src/main/java/com/example/demo/controllers/WorkerCRUD.java
@@ -5,22 +5,28 @@ import java.util.List;
 
 import com.example.demo.entity.Worker;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WorkerCRUD {
 
     @CrossOrigin(origins = "http://localhost:5000")
-    @PostMapping("/getworkers")
-    public List<Worker> getAll() throws Exception{
+    @GetMapping("/getworkers")
+    public @ResponseBody List<Worker> getAll() throws Exception{
 
         List<Worker> getList = new ArrayList<Worker>();
         Firestore database = FirestoreClient.getFirestore();
@@ -39,10 +45,27 @@ public class WorkerCRUD {
 
         Firestore database = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> result = database.collection("workers").document(worker.getName()).set(worker);
-        return "WORKER ADDED";
+        return result.get().getUpdateTime().toString();
 
     }
 
-    
+    @CrossOrigin(origins = "http://localhost:5000")
+    @DeleteMapping("/delete")
+    public String delete(@RequestParam(name = "id") int id) throws Exception{
+
+        Firestore database = FirestoreClient.getFirestore();
+        DocumentReference docRef = database.collection("workers").document(String.valueOf(id));
+        // ...
+        // future.get() blocks on response
+        DocumentSnapshot document = docRef.get().get();
+
+        if(document.exists()) {
+            docRef.delete();
+            return "DELETED";
+        }
+
+        else return "ERROR";
+
+    }
     
 }
--- svelte/src/Employees.svelte
+++ svelte/src/Employees.svelte
@@ -4,7 +4,53 @@
 
 <script>
     import { Table } from "sveltestrap/src";
-    import RemoveButton from "./RemoveButton.svelte";
+    import { Button } from "sveltestrap/src";
+
+    async function getList() {
+        const res = await fetch("http://localhost:8080/getworkers");
+        return res.json();
+    }
+
+    let promise = getList();
+
+    async function remove(ID) {
+        const res = await fetch("http://localhost:8080/delete?id=" + ID, {
+            method: 'DELETE',
+            headers: {'content-type': 'application/json'}
+        })
+
+        if(res.ok) {
+            promise = getList();
+        }
+
+    }
+
+    let toggle = 0;
+    let workerName = "";
+ 
+    const tog = () => {
+        toggle = 1;
+    }
+ 
+    async function reg() {
+        const res = await fetch("http://localhost:8080/addworker", {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify({
+                "name": workerName,
+                "pass": "12345"
+            })
+        })
+
+        const resp = await res.text();
+        console.log(resp);
+
+        if(res.ok) {
+            promise = getList();
+        }
+
+        toggle = 0;
+    }
 </script>
 
 <main>
@@ -13,15 +59,29 @@
         <thead>
             <th>#</th>
             <th>Name</th>
-            <th>Remove</th>
         </thead>
-        <tr>
-            <td>1</td>
-            <td>Sriram Balasubramanian</td>
-            <td><RemoveButton/></td>
-        </tr>
+        {#await promise}
+        <p>PLEASE WAIT</p>
+            
+        {:then workers}
+            {#each workers as w}
+            <tr>
+                <td>{w.id}</td>
+                <td>{w.name}</td>
+                <td><Button on:click={remove(w.id)}>Remove</Button></td>
+            </tr>
+            {/each} 
+            
+        {/await}
+        
     </Table>
-    <button>Add Worker</button>
+    <button on:click={tog}>Add Worker</button>
+    {#if toggle}
+        <p></p>
+        <p>Enter Worker Name:</p>
+        <input type="text" bind:value={workerName}><br><br>
+        <button on:click={reg}>Register Worker</button>
+    {/if}
 </main>
 
 <style>
